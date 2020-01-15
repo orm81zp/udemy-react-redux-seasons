f84d91bc58f4380639cd67e560b0a206bd429b77
roman onishchenko <ro@rollncode.com>
2020-01-15 17:07:32 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>Hi there!</div>
-  );
-};
+    this.state = {
+      lat: null
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude
+        })
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>Latitude: {this.state.lat}</div>
+    )
+  }
+}
 
 ReactDOM.render(
   <App/>,
