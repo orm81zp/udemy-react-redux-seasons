3be59a3db56fcc6258be87c11a020cd633b5babd
roman onishchenko <ro@rollncode.com>
2020-01-20 18:07:31 +0200

    next changes

--- src/index.js
+++ src/index.js
@@ -3,32 +3,16 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
-  }
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          lat: position.coords.latitude
-        })
-      },
-      (err) => {
-        this.setState({
-          errorMessage: err.message
-        })
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-  }
+  componentDidUpdate(prevProps, prevState, snapshot) {}
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
@@ -36,7 +20,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading...</div>
